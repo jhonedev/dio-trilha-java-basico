ebe7f0ac56aa035cae653fce6c873eef46c43c65
Baruc1996 <jonatan.borges2012@hotmail.com>
2024-06-13 15:46:59 -0300

    Adicionado um comentário para explicitar o exemplo

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -3,6 +3,7 @@ public class CaixaEletronico {
         double saldo = 25.0;
         double valorSolicitado = 17.0;
 
+        // Exemplo de controle de fluxo simples
         if (valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
         }
