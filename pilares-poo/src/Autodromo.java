82eaf83717a442ad6d45dbbec2ee1f8ea1c1f700
Baruc1996 <jonatan.borges2012@hotmail.com>
2024-06-22 23:34:41 -0300

    Exemplo de encapsulamento e herança

---
+++ pilares-poo/src/Autodromo.java
@@ -0,0 +1,11 @@
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro jeep = new Carro();
+
+        jeep.setChassi("555561");
+        jeep.ligar();
+
+        Moto z400 = new Moto();
+        z400.setChassi("454852");
+    }
+}
