5088bea90d91725690f48608e91f924e4f5e9f22
Baruc1996 <jonatan.borges2012@hotmail.com>
2024-07-06 15:19:33 -0300

    Testando os métodos e acrescentando hashcode e equals para codigoConvite ser unico e nao ter duplicados.

--- collections-java-api-2023/src/list/OperacoesBasicas/ConjuntoConvidados.java
+++ collections-java-api-2023/src/list/OperacoesBasicas/ConjuntoConvidados.java
@@ -35,4 +35,23 @@ public class ConjuntoConvidados {
      public void exibirConvidados() {
         System.out.println(convidadoSet);
      }
+
+     public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de convidados.");
+
+        conjuntoConvidados.adicionarConvidados("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidados("Convidado 2", 1235);
+        conjuntoConvidados.adicionarConvidados("Convidado 3", 1235);
+        conjuntoConvidados.adicionarConvidados("Convidado 4", 1236);
+
+        conjuntoConvidados.exibirConvidados();
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de convidados.");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1234);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de convidados");
+
+        conjuntoConvidados.exibirConvidados();
+     }
 }
