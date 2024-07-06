ffbbe2a98a90de13b0f043db48d50d33fed26c0d
Baruc1996 <jonatan.borges2012@hotmail.com>
2024-07-06 15:45:43 -0300

    Resolvendo os exercicios de Contato e AgendaContatos.

---
+++ collections-java-api-2023/src/list/Pesquisa/AgendaContatos.java
@@ -0,0 +1,45 @@
+package list.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    //atributo
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+     
+    //metodos
+    public void adicionarContato(String nome, int numero) {
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContato() {
+        System.out.println(contatoSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for(Contato contato : contatoSet) {
+            if (contato.getNome().startsWith(nome)) {
+                contatosPorNome.add(contato);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+
+        for(Contato contato : contatoSet) {
+            if (contato.getNome().equalsIgnoreCase(nome)) {
+                contato.setNumero(novoNumero);
+                contatoAtualizado = contato;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+}
