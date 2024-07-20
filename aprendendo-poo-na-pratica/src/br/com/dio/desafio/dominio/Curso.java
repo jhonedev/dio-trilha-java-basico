a134cb8f9f2b88bb4187f8b29b83478c0e656d84
Baruc1996 <jonatan.borges2012@hotmail.com>
2024-07-20 11:31:13 -0300

    Herança e polimorfismo, parte 1.

--- aprendendo-poo-na-pratica/src/br/com/dio/desafio/dominio/Curso.java
+++ aprendendo-poo-na-pratica/src/br/com/dio/desafio/dominio/Curso.java
@@ -1,9 +1,7 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo {
     //atributos -> Uma boa prática os atributos estarem private
-    private String titulo;
-    private String descricao;
     private int cargaHoraria;
 
     //construtor
@@ -12,18 +10,11 @@ public class Curso {
     }
 
     //metodos
-    public String getTitulo() {
-        return titulo;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+
     public int getCargaHoraria() {
         return cargaHoraria;
     }
@@ -32,9 +23,11 @@ public class Curso {
     }
 
     //toString
-    @Override
+    @Override 
     public String toString() {
-        return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+        return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
     }
+
+    
 
 }
--- aprendendo-poo-na-pratica/src/br/com/dio/desafio/dominio/Mentoria.java
+++ aprendendo-poo-na-pratica/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,33 +2,25 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo{
     //atributos -> Uma boa prática os atributos estarem private
-    private String titulo;
-    private String descricao;
     private LocalDate data;
 
     //construtor
     public Mentoria() {
-        
+
     }
 
     //metodos
-    public String getTitulo() {
-        return titulo;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
     }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+
     public LocalDate getData() {
         return data;
     }
+
     public void setData(LocalDate data) {
         this.data = data;
     }
@@ -36,7 +28,7 @@ public class Mentoria {
     //toString
     @Override
     public String toString() {
-        return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
+        return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + data + "]";
     }
     
 }
